a8bee3108263c90876f89defd070de872285b3ed
ECTheHunter <emirhancatalbas@windowslive.com>
2025-05-31 22:54:36 +0300

    .

--- Assets/Mutant.cs
+++ Assets/Mutant.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,46 +7,163 @@ public class Mutant : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Animator animator;
+    public Transform player;
+    public Light flashlight;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public Transform[] runAwayCheckpoints; // Assign in inspector: random points to flee to when hit by light
+    public float attackDistance = 1.5f;
+    public float attackAngle = 60f;
 
-    }
+    private Transform currentRunAwayTarget;
+    private bool isRunningAway = false;
 
-    // Update is called once per frame
+    // To track when we enter idle state
+    private bool isIdle = false;
+    private Coroutine idleCoroutine;
+
     void Update()
     {
-        navMeshAgent.destination = Vector3.zero;
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0); // 0 = Base Layer
-        if (navMeshAgent.velocity.magnitude > 0.1f)
+        bool flashlightOn = flashlight.gameObject.activeInHierarchy;
+        bool litByFlashlight = flashlightOn && IsInFlashlight();
+
+        if (isRunningAway)
+        {
+            // Run away until checkpoint is reached
+            navMeshAgent.SetDestination(currentRunAwayTarget.position);
+
+            if (ReachedDestination())
+            {
+                // Once checkpoint reached, stop running away and resume chasing player
+                isRunningAway = false;
+                currentRunAwayTarget = null;
+            }
+        }
+        else
+        {
+            if (litByFlashlight)
+            {
+                // Start running away to a random checkpoint
+                StartRunningAway();
+            }
+            else
+            {
+                // Chase the player
+                navMeshAgent.SetDestination(player.position);
+            }
+        }
+
+        UpdateAnimator();
+        CheckBackAttack();
+    }
+
+
+    bool IsInFlashlight()
+    {
+        Vector3 toEnemy = transform.position - flashlight.transform.position;
+        float angle = Vector3.Angle(flashlight.transform.forward, toEnemy);
+        float distance = toEnemy.magnitude;
+
+        return angle < flashlight.spotAngle / 2f && distance < flashlight.range;
+    }
+
+    void StartRunningAway()
+    {
+        if (runAwayCheckpoints.Length == 0)
+        {
+            Debug.LogWarning("No runAwayCheckpoints assigned!");
+            return;
+        }
+        currentRunAwayTarget = runAwayCheckpoints[Random.Range(0, runAwayCheckpoints.Length)];
+        isRunningAway = true;
+        navMeshAgent.SetDestination(currentRunAwayTarget.position);
+    }
+
+    bool ReachedDestination()
+    {
+        if (!navMeshAgent.pathPending)
+        {
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void UpdateAnimator()
+    {
+        float speed = navMeshAgent.velocity.magnitude;
+
+        // Running animation
+        if (speed > 0.1f)
         {
             animator.SetBool("isrunning", true);
+
+            // If moving, not idle, stop idle coroutine if running
+            if (isIdle)
+            {
+                isIdle = false;
+                if (idleCoroutine != null)
+                {
+                    StopCoroutine(idleCoroutine);
+                    idleCoroutine = null;
+                }
+            }
         }
         else
         {
             animator.SetBool("isrunning", false);
-        }
-        if (stateInfo.IsName("EmptyEntry"))
-        {
-            StartCoroutine(HandleEmptyEntryOnce()); ;
+
+            // If just became idle, start idle randomizer coroutine
+            if (!isIdle)
+            {
+                isIdle = true;
+                idleCoroutine = StartCoroutine(HandleRandomIdle());
+            }
         }
     }
-    IEnumerator HandleEmptyEntryOnce()
+
+    IEnumerator HandleRandomIdle()
     {
-
-        // Optional: wait for a short moment after animation starts
-        yield return null; // or: yield return new WaitForSeconds(0.1f);
-
-        int random = Random.Range(1, 4);
-        animator.SetInteger("idleValue", random);
-
-        // Wait until animation exits before allowing again
-        while (animator.GetCurrentAnimatorStateInfo(0).IsName("EmptyEntry"))
+        while (true)
         {
-            yield return null;
-        }
+            int randomIdleValue = Random.Range(1, 4);
+            animator.SetInteger("idleValue", randomIdleValue);
 
+            // Wait until leaving idle state (i.e., speed > 0.1f) or some time passes before changing again
+            // We'll check every 1-3 seconds for variety
+            float waitTime = Random.Range(1f, 3f);
+            float timer = 0f;
+
+            while (timer < waitTime)
+            {
+                if (navMeshAgent.velocity.magnitude > 0.1f)
+                    yield break; // stop coroutine when moving
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+        }
     }
 
+    void CheckBackAttack()
+    {
+        Vector3 toEnemy = (transform.position - player.position).normalized;
+        float angleFromBack = Vector3.Angle(player.forward, toEnemy);
+        float distToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (angleFromBack < attackAngle && distToPlayer < attackDistance)
+        {
+            AttackPlayer();
+        }
+    }
+
+    void AttackPlayer()
+    {
+        Debug.Log("Back attack triggered!");
+        // TODO: Add attack logic (damage, animation, etc.)
+    }
 }
