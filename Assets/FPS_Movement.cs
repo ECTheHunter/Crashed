84606e518b529a1cf236f81dc68836d38acd2fcf
ECTheHunter <emirhancatalbas@windowslive.com>
2025-05-31 13:31:08 +0300

    .

--- Assets/FPS_Movement.cs
+++ Assets/FPS_Movement.cs
@@ -7,6 +7,14 @@ public class FPS_Movement : MonoBehaviour
     float xrotation = 0;
     public GameObject playercamera;
     public CharacterController controller;
+    public float speed = 12f;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public Transform groundcheck;
+    public float grounddistance = 0.4f;
+    public LayerMask groundmask;
+    bool isgrounded;
+    public float jumpheight = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +24,11 @@ public class FPS_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isgrounded = Physics.CheckSphere(groundcheck.position, grounddistance, groundmask);
+        if (isgrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
         float mousex = Input.GetAxis("Mouse X") * mousesensitivity * Time.deltaTime;
         float mousey = Input.GetAxis("Mouse Y") * mousesensitivity * Time.deltaTime;
         xrotation -= mousey;
@@ -25,6 +38,12 @@ public class FPS_Movement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move);
+        controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetButtonDown("Jump") && isgrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
