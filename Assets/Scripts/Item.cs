21eaf759cedd9fc172ec9ba6a3def8eccc8cf824
ECTheHunter <emirhancatalbas@windowslive.com>
2025-06-01 19:02:14 +0300

    .

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,15 +5,15 @@ public class Item : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
+        print(other.gameObject.name);
         if (other.gameObject.tag == "Player")
         {
-            print("YOOOOOOOOOOOOOO");
             other.gameObject.GetComponent<CharacterModel>().IncreaseAclik(15f);
             other.gameObject.GetComponent<CharacterModel>().IncreaseOksijen(10f);
             other.gameObject.GetComponent<CharacterModel>().IncreaseSusuzluk(15f);
             other.gameObject.GetComponent<FPS_Movement>().lightsource = 15f;
             Destroy(gameObject);
-            
+
         }
     }
 
