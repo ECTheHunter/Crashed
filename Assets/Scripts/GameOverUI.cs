880799c3dd9395a123c2d0135e24e235670c4d00
Kagawashi <muratyer1005@hotmail.com>
2025-06-01 18:24:41 +0300

    Game over UI and Timer
    Now game over ui active and timer will timing

---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+
+    public GameObject GameOver;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("ölecek");
+            gameOver();
+            Debug.Log("öldü");
+        }
+    }
+    public void gameOver()
+    {
+        GameOver.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None; // Unlock the cursor
+        Cursor.visible = true; // Make the cursor visible
+    }
+
+
+    public void QuitButton()
+    {
+        
+         Application.Quit();
+       
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Test");
+        Time.timeScale = 1f;
+
+    }
+
+
+}
